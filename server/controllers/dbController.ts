43d36d85cef18858702653a02961175133272878
Sterling Deng <sterlingdeng@gmail.com>
2019-04-25 09:32:30 -0700

    initial redis upload

--- server/controllers/dbController.ts
+++ server/controllers/dbController.ts
@@ -4,6 +4,8 @@ import { QueryResult } from "pg";
 interface IDbReader<T> {
   Get: (f: ISearchFilter) => Promise<T>;
   GetTopTen: () => Promise<T>;
+  GetZipCodes: () => Promise<string[]>;
+  GetRestaurantNames: () => Promise<string[]>;
 }
 
 export interface ISearchFilter {
@@ -21,9 +23,7 @@ export const dbReader: IDbReader<QueryResult> = {
         Pool.query(
           "SELECT * FROM restaurants where name not like 'KFC' and name not like 'Pizza Hut' and name not like 'Church%' and name not like 'Wing%' and name not like 'Domino%' and name not like 'Popeyes%' and name not like 'Vons%' and name not like '%Pizza%' limit 100;"
         )
-          .then(result => {
-            resolve(result);
-          })
+          .then(result => resolve(result))
           .catch(err => reject(err));
       } else {
         const { offset, zip_code, name, count } = filters;
@@ -51,9 +51,7 @@ export const dbReader: IDbReader<QueryResult> = {
         }
 
         Pool.query(modifiedQueryString)
-          .then(result => {
-            resolve(result);
-          })
+          .then(result => resolve(result))
           .catch(err => reject(err));
       }
     });
@@ -61,19 +59,56 @@ export const dbReader: IDbReader<QueryResult> = {
   GetTopTen: () => {
     return new Promise((resolve, reject) => {
       const queryString = `
-      SELECT name, price, address1, address2, city, zip_code, image_url, AVG(spice) as avg_spice, AVG(crunch) as avg_spice, AVG(flavor) as avg_flavor, AVG(temp) as avg_temp, AVG(size) as avg_size, (AVG(crunch) + AVG(spice) + avg(flavor) +  avg(temp) + avg(size))/5.0 as overall_avg  
+      SELECT restaurants._id, name, price, address1, address2, city, zip_code, image_url, AVG(spice) as avg_spice, AVG(crunch) as avg_crunch, AVG(flavor) as avg_flavor, AVG(temp) as avg_temp, AVG(size) as avg_size, (AVG(crunch) + AVG(spice) + avg(flavor) +  avg(temp) + avg(size))/5.0 as overall_avg  
       FROM ratings
       INNER JOIN restaurants
       ON restaurants._id=ratings.restaurant_id              
-      GROUP BY name, price, address1, address2, city, zip_code, image_url
+      GROUP BY name, restaurants._id, price, address1, address2, city, zip_code, image_url
       ORDER BY overall_avg desc
       LIMIT 10;
       `;
       Pool.query(queryString)
         .then(res => {
+          console.log(`[POSTGRES] Successfully retrieved TopTen data`);
           resolve(res);
         })
         .catch(err => {
+          console.error(`[POSTGRES] Failed to retrieve TopTen data`);
+          reject(err);
+        });
+    });
+  },
+
+  GetZipCodes: () => {
+    return new Promise((resolve, reject) => {
+      const queryString = `
+       SELECT distinct zip_code from restaurants;
+      `;
+      Pool.query(queryString)
+        .then(res => {
+          console.log(`[POSTGRES] Successfully retrieved zipcode data`);
+          resolve(res.rows.map(entry => entry["zip_code"]));
+        })
+        .catch(err => {
+          console.error(`[POSTGRES] Failed to retrieve zipcode data`);
+          reject(err);
+        });
+    });
+  },
+
+  GetRestaurantNames: () => {
+    return new Promise((resolve, reject) => {
+      const queryString = `
+      SELECT distinct name from restaurants;
+      `;
+
+      Pool.query(queryString)
+        .then(res => {
+          console.log(`[POSTGRES] Successfully retrieved restaurant names data`);
+          resolve(res.rows.map(entry => entry["name"]));
+        })
+        .catch(err => {
+          console.error(`[POSTGRES] Failed to retrieve restaurant names data`);
           reject(err);
         });
     });
--- server/web/server.ts
+++ server/web/server.ts
@@ -1,9 +1,13 @@
 import * as express from "express";
 import { Request, Response, NextFunction } from "express";
-import { readController } from "../controllers";
+import { readController, cacheController } from "../controllers";
+import client from "../db/redisConnect";
 
 const app = express();
 const PORT = 3000;
+
+// initialization script
+client.flushall();
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("hello");
@@ -12,6 +16,12 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
 app.get("/getdata", readController.getData);
 
 app.get("/topten", readController.getTopTen);
+app.get(
+  "/initialize",
+  cacheController.checkCache,
+  readController.getInitialData,
+  cacheController.writeCache
+);
 
 app.get("/writedata");
 
